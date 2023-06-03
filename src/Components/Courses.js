cfad46dbce4cb5207ebf539654ca160065e23f57
girish4016 <115746976+girish4016@users.noreply.github.com>
2023-06-03 13:55:26 +0530

    Update Courses.js

--- src/Components/Courses.js
+++ src/Components/Courses.js
@@ -1,26 +1,23 @@
-import { useEffect, useState } from "react"
 import Card from "./Card"
 
 const Courses = ( { apiData , Criteria} ) => {
-    const [allElements, setAllElements] = useState([]);
     
     function makeCourseArray(){
         const Arr = [];
+        if(Criteria!=='All')return apiData.data[Criteria];
         Object.keys(apiData.data).forEach( (KEY ) => {
-            if(Criteria==='All' || Criteria ===  KEY){
-                apiData.data[KEY].map((elements) => {
-                    return Arr.push(elements);
-                } )
-            }
+            apiData.data[KEY].map((elements) => {
+                return Arr.push(elements);
+            } )
          })
-        setAllElements(Arr);
+        return Arr;
     }
-
-    useEffect(makeCourseArray,[Criteria,apiData] );
+    makeCourseArray();
+    
 
     return (<div className="flex max-w-[1200px] flex-row flex-wrap gap-4 justify-center text-center pb-8
     " >{
-        allElements.map( element => {return( <Card key={element.id} element = {element} ></Card> )})
+        makeCourseArray().map( element => {return( <Card key={element.id} element = {element} ></Card> )})
     }</div>)
 }
 export default Courses;
